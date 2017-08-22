f28a18e84e3ea7ad849dc4c2de64bea4905064ba
Daniel Zhu <danzhu24@hotmail.com>
2017-08-22 13:04:01 -0400

    Fix weak vtables

---
+++ src/exception.cc
@@ -0,0 +1,5 @@
+#include "exception.h"
+
+Fin::RuntimeError::~RuntimeError() noexcept {}
+
+Fin::AllocationError::~AllocationError() noexcept {}
--- src/exception.h
+++ src/exception.h
@@ -8,11 +8,27 @@ namespace Fin
 class RuntimeError : public std::runtime_error
 {
     using std::runtime_error::runtime_error;
+
+public:
+    RuntimeError(const RuntimeError &other) = default;
+    RuntimeError(RuntimeError &&other) = default;
+    ~RuntimeError() noexcept;
+
+    RuntimeError &operator=(const RuntimeError &other) = default;
+    RuntimeError &operator=(RuntimeError &&other) = default;
 };
 
 class AllocationError : public std::bad_alloc
 {
     using std::bad_alloc::bad_alloc;
+
+public:
+    AllocationError(const AllocationError &other) = default;
+    AllocationError(AllocationError &&other) = default;
+    ~AllocationError() noexcept;
+
+    AllocationError &operator=(const AllocationError &other) = default;
+    AllocationError &operator=(AllocationError &&other) = default;
 };
 }
 
